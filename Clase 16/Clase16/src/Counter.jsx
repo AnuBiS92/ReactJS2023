2ee73f4a66b9c60436eb253099ea3561f0eedecf
Emiliano Alvarez <64234452+AnuBiS92@users.noreply.github.com>
2023-10-09 23:20:27 -0300

    Clase 16 - Adición de React Bootstrap

--- Clase 16/Clase16/src/Counter.jsx
+++ Clase 16/Clase16/src/Counter.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Collapse from "react-bootstrap/Collapse";
 export const Counter = () => {
   const [contador, setContador] = useState(0); //Controla el estado de un elemento. Los cambios a lo largo del tiempo.
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     //Para que ocurra algo cada vez que cambia el estado de una variable.
     console.log("Cambió el contador"); //¿Qué tiene que pasar cuando cambie la variable que elijamos?
@@ -10,12 +12,34 @@ export const Counter = () => {
   return (
     <>
       <p>Contador:{contador}</p>
-      <button onClick={() => setContador(contador + 1)} variant="success">
-        +
-      </button>
-      <button onClick={() => setContador(contador - 1)} variant="danger">
-        -
-      </button>
+      <Button onClick={() => setContador(contador + 1)} variant="success">
+        <b>SUMAR</b>
+      </Button>
+      <Button onClick={() => setContador(contador - 1)} variant="danger">
+        <b>RESTAR</b>
+      </Button>
+      <br />
+      <br />
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        click
+      </Button>
+      <Collapse in={open}>
+        <Card style={{ width: "18rem" }} id="example-collapse-text">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Collapse>
     </>
   );
 };
